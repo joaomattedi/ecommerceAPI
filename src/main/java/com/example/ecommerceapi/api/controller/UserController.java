698ce167fe65a00599f811398f81043232df67e6
João Mateus <69877286+joaomattedi@users.noreply.github.com>
2024-07-02 22:14:33 -0300

    wip: register user route

--- src/main/java/com/example/ecommerceapi/api/controller/UserController.java
+++ src/main/java/com/example/ecommerceapi/api/controller/UserController.java
@@ -1,2 +1,26 @@
-package com.example.ecommerceapi.api.controller;public class UserController {
+package com.example.ecommerceapi.api.controller;
+
+import com.example.ecommerceapi.api.dto.UserDTO;
+import com.example.ecommerceapi.api.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private UserService _userService;
+
+    public void UserController(UserService userService) {
+        _userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity registerUser(@RequestBody UserDTO userDto) {
+
+        return ResponseEntity.ok().build();
+    }
+
 }
--- src/main/java/com/example/ecommerceapi/api/dto/UserDTO.java
+++ src/main/java/com/example/ecommerceapi/api/dto/UserDTO.java
@@ -1,2 +1,14 @@
-package com.example.ecommerceapi.api.dto;public class UserDTO {
+package com.example.ecommerceapi.api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String cellphoneNumber;
 }
